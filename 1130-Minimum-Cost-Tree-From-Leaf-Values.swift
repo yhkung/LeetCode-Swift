592a7cae05c74b752570636ad9973d6ca9d17bd6
YH Kung <yh@17.media>
2019-11-30 01:32:25 +0800

    Add #1130

---
+++ 1130-Minimum-Cost-Tree-From-Leaf-Values.swift
@@ -0,0 +1,20 @@
+/// 1130. Minimum Cost Tree From Leaf Values
+/// https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+class Solution {
+    func mctFromLeafValues(_ arr: [Int]) -> Int {
+        var res = 0
+        var stack = [Int]()
+        stack.append(Int.max)
+        for v in arr {
+            while stack.last! <= v {
+                let mid = stack.removeLast()
+                res += mid * min(stack.last!, v)
+            }
+            stack.append(v)
+        }        
+        while stack.count > 2 {
+            res += stack.removeLast() * stack.last!
+        }
+        return res
+    }
+}
