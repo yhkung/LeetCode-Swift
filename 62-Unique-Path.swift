fb4b806d5ffec3bd0eb75a31909b4632998e42df
yhkung <bowser.kung@gmail.com>
2019-12-29 19:15:00 +0800

    #62 Optimize memory useage

--- 62-Unique-Path.swift
+++ 62-Unique-Path.swift
@@ -2,7 +2,7 @@
 /// - Time Complexity: O(m*n)
 /// - Space Complexity: O(m*n)
 func uniquePaths(_ m: Int, _ n: Int) -> Int {
-    guard m > 0, n > 0 else { return 0}
+    guard m > 0, n > 0 else { return 0 }
     var grid = Array(repeating: Array(repeating: 1, count: m), count: n)
     for row in 1..<n {
         for col in 1..<m {
@@ -12,4 +12,38 @@ func uniquePaths(_ m: Int, _ n: Int) -> Int {
         }
     }
     return grid[n-1][m-1]
+}
+
+/// 62. Unique Path - Reduce memory usage #1
+/// - Time Complexity: O(m*n)
+/// - Space Complexity: O(n)
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        guard m > 0, n > 0 else { return 0 }
+        var cur = Array(repeating: 1, count: n)
+        var pre = Array(repeating: 1, count: n)
+        for i in 1..<m {
+            for j in 1..<n {
+                cur[j] = pre[j] + cur[j-1]
+            }
+            pre = cur
+        }
+        return cur[n-1]
+    }
+}
+
+/// 62. Unique Path - Reduce memory usage #2
+/// - Time Complexity: O(m*n)
+/// - Space Complexity: O(1)
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        guard m > 0, n > 0 else { return 0 }
+        var cur = Array(repeating: 1, count: n)
+        for i in 1..<m {
+            for j in 1..<n {
+                cur[j] += cur[j-1]
+            }
+        }
+        return cur[n-1]
+    }
 }
