9ee900989a1411ff4f0a8fd6aaa9e98ed9cf43a9
yhkung <bowser.kung@gmail.com>
2020-01-05 18:00:53 +0800

    Add #438, sliding window

---
+++ 438-Find-All-Anagrams-in-a-String.swift
@@ -0,0 +1,36 @@
+/// 438. Find All Anagrams in a String
+/// - Approach: Sliding Window
+/// - Time Complexity: O(n)
+class Solution {
+    func findAnagrams(_ s: String, _ p: String) -> [Int] {
+        let s = Array(s)
+        let p = Array(p)
+        var sDict = [Character: Int]()
+        var pDict = [Character: Int]()
+        var results = [Int]()
+        for c in p {
+            pDict[c, default: 0] += 1
+        }
+
+        for i in 0..<s.count {
+            if i >= p.count {
+                let firstChar = s[i - p.count]
+                if let countForChar = sDict[firstChar] {
+                    if countForChar == 1 {
+                        sDict[firstChar] = nil
+                    } else {
+                        sDict[firstChar] = countForChar - 1
+                    }
+                }
+            }
+
+            sDict[s[i], default: 0] += 1
+
+            if sDict == pDict {
+                results.append(i - p.count + 1)
+            }
+        }
+
+        return results
+    }
+}
