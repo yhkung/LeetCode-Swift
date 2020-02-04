86411d166d8b6304b12740a23191dca9e18c7bba
yhkung <bowser.kung@gmail.com>
2020-02-04 22:48:36 +0800

    Rewrite #076

--- 076-Minimum-Window-Substring.swift
+++ 076-Minimum-Window-Substring.swift
@@ -4,44 +4,37 @@ class Solution {
     func minWindow(_ s: String, _ t: String) -> String {
         guard !s.isEmpty && !t.isEmpty else { return "" }
         let s = Array(s)
-        let t = Array(t)
         var start = 0
         var end = 0
         var minStart = 0
         var minLength = Int.max
-        var dict = [Character: Int]()
-        t.forEach { dict[$0, default: 0] += 1 }
+        var dict = t.reduce(into:[Character: Int]()) { $0[$1, default: 0] += 1 }
         var counter = t.count
 
-        // Move end to find a valid window.
         while end < s.count {
-            let c = s[end]
-
-            // If char in s exists in t, decrease counter
-            if let count = dict[c], count > 0 {
+            if let value = dict[s[end]], value > 0 {
                 counter -= 1
             }
 
-            // Decrease dict[c]. If char does not exist in t, dict[c] will be negative.
-            dict[c, default: 0] -= 1
-
+            dict[s[end], default: 0] -= 1
             end += 1
 
-            // When we found a valid window, move start to find smaller window.
             while counter == 0 {
                 if end - start < minLength {
-                    minStart = start
                     minLength = end - start
+                    minStart = start
                 }
-                let c = s[start]
-                dict[c, default: 0] += 1
-                if let count = dict[c], count > 0 {
+
+                dict[s[start], default: 0] += 1
+
+                if let value = dict[s[start]], value > 0 {
                     counter += 1
                 }
+
                 start += 1
             }
         }
 
-        return minLength == Int.max ? "" : String(s[minStart..<minStart+minLength])
+        return minLength == Int.max ? "" : String(s[minStart..<minStart + minLength])
     }
 }
