5475d545390f6bf520f9e55f1008798a23568814
yhkung <bowser.kung@gmail.com>
2020-02-04 20:52:32 +0800

    Add counter method for #242

--- 242-Valid-Anagram.swift
+++ 242-Valid-Anagram.swift
@@ -8,3 +8,18 @@ class Solution {
                t.reduce([Character: Int]()) { $0[$1, default: 0] += 1 }
     }
 }
+
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { return false }
+        var dict = t.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
+        var counter = t.count
+        for c in s {
+            if let count = dict[c], count > 0 {
+                dict[c, default: 0] -= 1
+                counter -= 1
+            }
+        }
+        return counter == 0
+    }
+}
