e44b137090a8bf455757252031f2be9616680fe9
yhkung <bowser.kung@gmail.com>
2020-02-04 20:44:09 +0800

    Improve 242 with reduce(into:) function

--- 242-Valid-Anagram.swift
+++ 242-Valid-Anagram.swift
@@ -4,10 +4,7 @@
 /// - Space Complexity: O(1)
 class Solution {
     func isAnagram(_ s: String, _ t: String) -> Bool {
-        var sDict: [Character: Int] = [:]
-        var tDict: [Character: Int] = [:]
-        s.forEach { sDict[$0, default: 0] += 1 }
-        t.forEach { tDict[$0, default: 0] += 1 }
-        return sDict == tDict
+        return s.reduce([Character: Int]()) { $0[$1, default: 0] += 1 } ==
+               t.reduce([Character: Int]()) { $0[$1, default: 0] += 1 }
     }
 }
