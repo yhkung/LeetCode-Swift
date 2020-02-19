b59e963e4a5d83b9b3a7f54b040ac3b69de54023
yhkung <bowser.kung@gmail.com>
2020-02-20 01:26:10 +0800

    Rewrite #286

--- 286-Walls-and-Gates.swift
+++ 286-Walls-and-Gates.swift
@@ -8,37 +8,48 @@ class Solution {
         (0, 1),
         (0, -1)
     ]
+
+    private let WALL = -1
     private let GATE = 0
-    private let EMPTY = 2147483647
+    private let EMPTY_ROOM = 2147483647
 
     func wallsAndGates(_ rooms: inout [[Int]]) {
-        guard rooms.count > 0 else { return }
+        guard !rooms.isEmpty, let first = rooms.first else { return }
         var m = rooms.count
-        var n = rooms[0].count
+        var n = first.count
 
-        var queue = [(Int, Int)]()
-        for row in 0..<m {
-            for col in 0..<n {
-                if rooms[row][col] == GATE {
-                    queue.append((row, col))
+        var queue: [(Int, Int)] = []
+        for i in 0..<m {
+            for j in 0..<n {
+                if rooms[i][j] == GATE {
+                    queue.append((i, j))
                 }
             }
         }
 
+        var steps = 0
         while !queue.isEmpty {
-            let point = queue.removeFirst()
-            let row = point.0
-            let col = point.1
-            for direction in directions {
-                let r = row + direction.0
-                let c = col + direction.1
-                if r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY {
-                    continue
+            for _ in 0..<queue.count {
+                var cur = queue.removeFirst()
+                let row = cur.0
+                let col = cur.1
+
+                if rooms[row][col] == EMPTY_ROOM {
+                    rooms[row][col] = steps
                 }
-                rooms[r][c] = rooms[row][col] + 1
-                queue.append((r, c))
+
+                if rooms[row][col] != WALL {
+                    for dir in directions {
+                        let next = (row + dir.0, col + dir.1)
+                        let isValid = next.0 >= 0 && next.0 < m &&
+                                      next.1 >= 0 && next.1 < n
+                        if isValid && rooms[next.0][next.1] == EMPTY_ROOM {
+                            queue.append(next)
+                        }
+                    }
+                }
             }
+            steps += 1
         }
     }
-
 }
