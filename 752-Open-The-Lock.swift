cd98f4717ab3b5dd117e09ad2710d4abbdf92869
yhkung <bowser.kung@gmail.com>
2020-02-22 00:45:48 +0800

    Add #752

---
+++ 752-Open-The-Lock.swift
@@ -0,0 +1,40 @@
+/// 752 - Open The Lock
+/// - Link: https://leetcode.com/problems/open-the-lock/
+/// - Approach: BFS
+class Solution {
+    func openLock(_ deadends: [String], _ target: String) -> Int {
+        var deadends: Set<[Int]> = Set(deadends.map { Array($0).compactMap { Int("\($0)") } })
+        var target: [Int] = Array(target).compactMap { Int("\($0)") }
+        var turns: Int = 0
+        var queue: [[Int]] = []
+        var visited: Set<[Int]> = deadends
+
+        if !visited.contains([0, 0, 0, 0]) {
+            queue.append([0, 0, 0, 0])
+            visited.insert([0, 0, 0, 0])
+        }
+
+        while !queue.isEmpty {
+            for _ in 0..<queue.count {
+                let cur = queue.removeFirst()
+                if cur == target {
+                    return turns
+                }
+
+                for i in 0...3 {
+                    for d in stride(from: -1, through: 1, by: 2) {
+                        var neighbor = cur
+                        neighbor[i] = ((neighbor[i] + d) + 10) % 10
+
+                        if !visited.contains(neighbor) {
+                            queue.append(neighbor)
+                            visited.insert(neighbor)
+                        }
+                    }
+                }
+            }
+            turns += 1
+        }
+        return -1
+    }
+}
