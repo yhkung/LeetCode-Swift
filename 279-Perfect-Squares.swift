fc3c8122e1ecd04e1b3128514f161cf043c0832f
yhkung <bowser.kung@gmail.com>
2020-02-22 11:47:25 +0800

    Add #279, BFS

---
+++ 279-Perfect-Squares.swift
@@ -0,0 +1,33 @@
+/// 279 - Perfect Squares
+/// - Approach: BFS
+class Solution {
+    func numSquares(_ n: Int) -> Int {
+        var visited: Set<Int> = Set()
+        var queue: [Int] = []
+
+        queue.append(0)
+
+        var steps = 0
+        while !queue.isEmpty {
+            steps += 1
+
+            for _ in 0..<queue.count {
+                let cur = queue.removeFirst()
+                for i in 1..<n {
+                    let sum = cur + (i * i)
+                    if sum == n {
+                        return steps
+                    } else if sum > n {
+                        break
+                    } else {
+                        if !visited.contains(sum) {
+                            queue.append(sum)
+                            visited.insert(sum)
+                        }
+                    }
+                }
+            }
+        }
+        return steps
+    }
+}
