cf1a89c494c408bd4574d6d80ed0c435f2df2297
yhkung <bowser.kung@gmail.com>
2020-02-28 01:17:33 +0800

    Add #070, make steps as paramter

--- 070-Climbing-Stairs.swift
+++ 070-Climbing-Stairs.swift
@@ -1,3 +1,31 @@
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        return climbStairs(n, maxSteps: 2)
+    }
+
+    private func climbStairs(_ n: Int, maxSteps: Int) -> Int {
+
+        /// We only needs (maxSteps + 1) size array to keep tracking previous number of ways
+        let size = maxSteps + 1
+
+        /// dp is an array that holds the number of disctincts ways
+        var dp: [Int] = [Int](repeating: 0, count: size)
+
+        dp[0] = 1
+        for i in 1...n {
+
+            /// dp[n] = dp[n - 1] + dp[n - 2] +... dp[n - maxSteps]
+            var sum = 0
+            for step in 1...maxSteps {
+                guard i >= step else { continue }
+                sum += dp[(i - step) % size]
+            }
+            dp[i % size] = sum
+        }
+        return dp[n % size]
+    }
+}
+
 /// 70. Climbing Stairs
 /// - Approach: Bottom-Up
 /// - Time Complexity: O(n)
