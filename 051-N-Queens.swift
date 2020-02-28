24bd47d1c7504b36bd818f985dca26b0f0b515f3
yhkung <bowser.kung@gmail.com>
2020-02-28 11:11:24 +0800

    Add #051 with stack

--- 051-N-Queens.swift
+++ 051-N-Queens.swift
@@ -1,3 +1,6 @@
+/// 051 - N Queens
+/// - Link: https://leetcode.com/problems/n-queens/
+/// - Approach: Backtracking - Recursion
 class Solution {
     func solveNQueens(_ n: Int) -> [[String]] {
         var rows: [Int] = Array(repeating: 0, count: n)
@@ -45,4 +48,64 @@ class Solution {
         return output
     }
 
+}
+
+/// 051 - N Queens
+/// - Link: https://leetcode.com/problems/n-queens/
+/// - Approach: Backtracking - Stack
+class Solution {
+    func solveNQueens(_ n: Int) -> [[String]] {
+        var results: [[String]] = []
+        var rows: [Int] = Array(repeating: 0, count: n)
+        var stack: [(rows: [Int], index: Int)] = [(rows, 0)]
+
+        while !stack.isEmpty {
+            let cur = stack.removeLast()
+            let curRows = cur.0
+            let curIndex = cur.1
+
+            if curIndex == curRows.count {
+                let board = generateBoard(curRows)
+                results.append(board)
+                continue
+            }
+
+            for i in 0..<n {
+                var nextRows = curRows
+                nextRows[curIndex] = i
+                if !threatDetected(nextRows, curIndex) {
+                    stack.append((nextRows, curIndex + 1))
+                }
+            }
+        }
+
+        return results
+    }
+
+    private func threatDetected(_ rows: [Int], _ index: Int) -> Bool {
+        var checkRow = index - 1
+        while checkRow >= 0 {
+            if rows[checkRow] == rows[index] {
+                return true
+            }
+
+            let dx = abs(rows[checkRow] - rows[index])
+            let dy = index - checkRow
+            if dx == dy {
+                return true
+            }
+            checkRow -= 1
+        }
+        return false
+    }
+
+    private func generateBoard(_ rows: [Int]) -> [String] {
+        var results: [String] = []
+        for r in rows {
+            var str = Array(repeating: ".", count: rows.count)
+            str[r] = "Q"
+            results.append(str.joined())
+        }
+        return results
+    }
 }
