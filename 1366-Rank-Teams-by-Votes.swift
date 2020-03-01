a8fbfda08e7cc7d1bf362648d5a52bde1cf7c6bf
yhkung <bowser.kung@gmail.com>
2020-03-01 14:42:33 +0800

    Add #1366

---
+++ 1366-Rank-Teams-by-Votes.swift
@@ -0,0 +1,30 @@
+/// 1366 - Rank Teams by Votes
+/// - Link: https://leetcode.com/problems/rank-teams-by-votes/
+class Solution {
+    func rankTeams(_ votes: [String]) -> String {
+        guard votes.count > 1 else {
+            return votes.isEmpty ? "" : votes.first!
+        }
+        var ranks: [Character: [Int]] = [:]
+
+        for i in 0..<votes.count {
+            let vote = Array(votes[i])
+            let teamCount = vote.count
+            for j in 0..<teamCount {
+                let team = vote[j]
+                ranks[team, default: [Int](repeating: 0, count: teamCount)][j] += 1
+            }
+        }
+
+        var teams = Array(votes.first!)
+        teams.sort {
+            for rank in 0..<ranks.count {
+                if ranks[$0]![rank] != ranks[$1]![rank] {
+                    return ranks[$0]![rank] > ranks[$1]![rank]
+                }
+            }
+            return $0 < $1
+        }
+        return String(teams)
+    }
+}
