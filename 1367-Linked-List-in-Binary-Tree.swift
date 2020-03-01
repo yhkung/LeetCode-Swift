88edf6fbca96aa248f111d4c2ae5e710cb115a01
yhkung <bowser.kung@gmail.com>
2020-03-01 17:55:09 +0800

    Add #1367

--- 1367-Linked-List-in-Binary-Tree.swift
+++ 1367-Linked-List-in-Binary-Tree.swift
@@ -1,29 +1,5 @@
 /// 1367 - Linked List in Binary Tree
 /// - Link: https://leetcode.com/problems/linked-list-in-binary-tree/
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public var val: Int
- *     public var next: ListNode?
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.next = nil
- *     }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public var val: Int
- *     public var left: TreeNode?
- *     public var right: TreeNode?
- *     public init(_ val: Int) {
- *         self.val = val
- *         self.left = nil
- *         self.right = nil
- *     }
- * }
- */
 class Solution {
     func isSubPath(_ head: ListNode?, _ root: TreeNode?) -> Bool {
         if head == nil { return true }
