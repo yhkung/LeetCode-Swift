cd3bdebb9b1b166c4088f2a2e1a3eaef646fb58d
yhkung <bowser.kung@gmail.com>
2020-03-18 23:32:05 +0800

    Add #645 cyclic sort

--- 645-Set-Mismatch.swift
+++ 645-Set-Mismatch.swift
@@ -46,3 +46,31 @@ class Solution {
         return [duplicateNum, missingNum]
     }
 }
+
+/// - Cyclic Sort
+class Solution {
+    func findErrorNums(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        var i = 0
+        while i < nums.count {
+            var j = nums[i] - 1
+            if j < nums.count && nums[i] != nums[j] {
+                var tmp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = tmp
+            } else {
+                i += 1
+            }
+        }
+        var duplicateNum = 0
+        var missingNum = nums.count
+        for i in 0..<nums.count {
+            if nums[i] != i + 1 {
+                duplicateNum = nums[i]
+                missingNum = i + 1
+                break
+            }
+        }
+        return [duplicateNum, missingNum]
+    }
+}
