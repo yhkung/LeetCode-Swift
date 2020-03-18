6ecd142dc99c7485a6fc985da856fb474bd5e3ac
yhkung <bowser.kung@gmail.com>
2020-03-18 23:18:25 +0800

    Add #645

--- 645-Set-Mismatch.swift
+++ 645-Set-Mismatch.swift
@@ -1,5 +1,7 @@
 /// 645 - Set Mismatch
 /// - Link: https://leetcode.com/problems/set-mismatch/
+/// - Time: O(n)
+/// - Space: O(n)
 class Solution {
     func findErrorNums(_ nums: [Int]) -> [Int] {
         var set = Set<Int>()
@@ -17,5 +19,30 @@ class Solution {
         var sum = nums.reduce(0) { $0 + $1 } - duplicate
         missing = expect - sum
         return [duplicate, missing]
+    }
+}
+
+/// 645 - Set Mismatch
+/// - Link: https://leetcode.com/problems/set-mismatch/
+/// - Time: O(n)
+/// - Space: O(1)
+class Solution {
+    func findErrorNums(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        var duplicateNum = 0
+        var missingNum = 0
+        for i in nums {
+            if nums[i - 1] < 0 {
+                duplicateNum = i
+            } else {
+                nums[i - 1] *= -1
+            }
+        }
+        for i in 0..<nums.count {
+            if nums[i] > 0 {
+                missingNum = i + 1
+            }
+        }
+        return [duplicateNum, missingNum]
     }
 }
