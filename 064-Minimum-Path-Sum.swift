9586921f90a4fb5f1f87c692500aedd4fc7f1907
yhkung <yh@17.media>
2020-04-18 15:37:30 +0800

    Practice 064

--- 064-Minimum-Path-Sum.swift
+++ 064-Minimum-Path-Sum.swift
@@ -4,6 +4,27 @@
 /// - Space Complexity: O(n)
 class Solution {
     func minPathSum(_ grid: [[Int]]) -> Int {
+        var m = grid.count
+        var n = grid.first?.count ?? 0
+        var sum: [[Int]] = grid
+        for i in 0..<m {
+            for j in 0..<n {
+                if i == 0 && j == 0 { continue }
+                if i == 0 && j > 0 {
+                    sum[i][j] += sum[i][j - 1]
+                } else if i > 0 && j == 0 {
+                    sum[i][j] += sum[i - 1][0]
+                } else {
+                    sum[i][j] += min(sum[i - 1][j], sum[i][j - 1])
+                }
+            }
+        }
+        return sum[m - 1][n - 1]
+    }
+}
+
+class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
         guard !grid.isEmpty else { return 0 }
         guard let first = grid.first, !first.isEmpty else { return 0 }
         let m = grid.count
