05db007fa8a45c4514c644ec1935f0086e1b262e
yhkung <yh@17.media>
2020-05-11 04:05:32 +0800

    Add complexity analysis for #997

--- 997-Find-The-Judge.swift
+++ 997-Find-The-Judge.swift
@@ -1,30 +1,7 @@
 /// 997 - Find The Town Judge
 /// - Link: https://leetcode.com/problems/find-the-town-judge/
-class Solution {
-    func findJudge(_ N: Int, _ trust: [[Int]]) -> Int {
-        var dict = [Int: [Int]]()
-        for i in 0..<N {
-            dict[i+1] = [i+1]
-        }
-        for t in trust {
-            let person = t[0]
-            let trustee = t[1]
-            dict.removeValue(forKey: person)
-            if let people = dict[trustee] {
-                dict[trustee] = people + [person]
-            }
-        }
-        var judge = -1
-        for (trustee, people) in dict {
-            if people.count == N {
-                judge = trustee
-                break
-            }
-        }
-        return judge
-    }
-}
-
+/// - Time: O(E), Recall that N is the number of people, and E is the number of edges (trust relationships).
+/// - Space: O(N)
 class Solution {
     func findJudge(_ N: Int, _ trust: [[Int]]) -> Int {
         if trust.count < N - 1 {
