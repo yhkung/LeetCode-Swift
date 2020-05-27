e8576fd5bcd3139a74e09b87ae55dbbda34086cc
yhkung <yh@17.media>
2020-05-28 00:35:31 +0800

    Add #886 bfs approach

--- 886-Possible-Bipartition.swift
+++ 886-Possible-Bipartition.swift
@@ -20,16 +20,34 @@ class Solution {
     }
 
     private func dfs(_ node: Int, _ colors: inout [Int], _ graph: [[Int]]) -> Bool {
-        for i in 0..<graph[node].count {
-            let otherNode = graph[node][i]
-            if colors[otherNode] != 0 {
-                if colors[otherNode] == colors[node] {
+        for neightbor in graph[node] {
+            if colors[neightbor] != 0 {
+                if colors[neightbor] == colors[node] {
                     return false
                 }
             } else {
-                colors[otherNode] = colors[node] == 1 ? -1 : 1
-                if !dfs(otherNode, &colors, graph) {
+                colors[neightbor] = colors[node] == 1 ? -1 : 1
+                if !dfs(neightbor, &colors, graph) {
                     return false
+                }
+            }
+        }
+        return true
+    }
+
+    private func bfs(_ node: Int, _ colors: inout [Int], _ graph: [[Int]]) -> Bool {
+        var graph = graph
+        var queue = [node]
+        while !queue.isEmpty {
+            let first = queue.removeFirst()
+            for neighbor in graph[first] {
+                if colors[neighbor] != 0 {
+                    if colors[neighbor] == colors[first] {
+                        return false
+                    }
+                } else {
+                    colors[neighbor] = colors[first] == 1 ? -1 : 1
+                    queue.append(neighbor)
                 }
             }
         }
