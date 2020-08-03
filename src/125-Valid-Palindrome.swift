d071ffc1e29e2a518b9f205cea8d98beb943e837
yhkung <yh@17.media>
2020-08-03 23:23:39 +0800

    improve #125

--- src/125-Valid-Palindrome.swift
+++ src/125-Valid-Palindrome.swift
@@ -1,16 +1,30 @@
 /// 125. Valid Palindrome
 /// Time Complexity: O(n)
+extension Character {
+    var isLetterOrNumber: Bool {
+        return isLetter || isNumber
+    }
+}
+
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-        let s = Array(s.lowercased()).filter { $0.isLetter || $0.isNumber }
-        var left = 0
-        var right = s.count - 1
-        while right > left {
-            if s[left] != s[right] {
+        let s = Array(s)
+        var i = 0
+        var j = s.count - 1
+
+        while i < j {
+            while i < j && !s[i].isLetterOrNumber {
+                i += 1
+            }
+            while i < j && !s[j].isLetterOrNumber {
+                j -= 1
+            }
+
+            if s[i].lowercased() != s[j].lowercased() {
                 return false
             }
-            left += 1
-            right -= 1
+            i += 1
+            j -= 1
         }
         return true
     }
