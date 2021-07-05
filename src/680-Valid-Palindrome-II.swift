f91170f007efb5842ee2b45b277501e17f9082d4
YHKung <bowser.kung@gmail.com>
2021-07-05 17:22:58 +0800

    Practice problem #680

--- src/680-Valid-Palindrome-II.swift
+++ src/680-Valid-Palindrome-II.swift
@@ -29,3 +29,45 @@ class Solution {
         return true
     }
 }
+
+class Solution {
+    func validPalindrome(_ s: String) -> Bool {
+        guard !s.isEmpty else {
+            return false
+        }
+
+        let s = Array(s)
+        let pair = findDifference(0, s.count - 1, s)
+
+        if pair.left >= pair.right {
+            return true
+        }
+
+        return isPalindrome(pair.left + 1, pair.right, s) || isPalindrome(pair.left, pair.right - 1, s)
+    }
+
+    private func isPalindrome(_ left: Int, _ right: Int, _ s: [Character]) -> Bool {
+        guard !s.isEmpty else {
+            return false
+        }
+
+        let pair = findDifference(left, right, s)
+
+        return pair.left >= pair.right
+    }
+
+    private func findDifference(_ left: Int, _ right: Int, _ s: [Character]) -> (left: Int, right: Int) {
+        var left = left
+        var right = right
+
+        while left < right {
+            if s[left] != s[right] {
+                break
+            }
+            left += 1
+            right -= 1
+        }
+
+        return (left, right)
+    }
+}
