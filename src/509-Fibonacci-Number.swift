bb2b83537682406402a23e004ef51bea854d75f8
YHKung <bowser.kung@gmail.com>
2021-07-15 18:09:30 +0800

    Practice problem #509

--- src/509-Fibonacci-Number.swift
+++ src/509-Fibonacci-Number.swift
@@ -1,11 +1,47 @@
 class Solution {
-    func fib(_ N: Int) -> Int {
-        guard N > 1 else { return N }
-        var prev = 0 // f(0)
-        var curr = 1 // f(1)
-        for _ in 0..<N-1 {
-            (prev, curr) = (curr, curr + prev)
+    func fib(_ n: Int) -> Int {
+        if n == 0 {
+            return 0
         }
+        if n == 1 {
+            return 1
+        }
+
+        var fibonacci = Array(repeating: 0, count: n + 1)
+
+        // f(n)  - 0, 1, 1, 2, 3, 4
+        //
+        // index - 0, 1, 2, 3, 4, 5
+
+        fibonacci[0] = 0
+        fibonacci[1] = 1
+
+        for i in 2...n {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+        }
+
+        return fibonacci[n]
+    }
+}
+
+class Solution {
+    func fib(_ n: Int) -> Int {
+        if n == 0 {
+            return 0
+        }
+        if n == 1 {
+            return 1
+        }
+
+        var prev = 0
+        var curr = 1
+
+        for _ in 0..<n-1 {
+            let sum = prev + curr
+            prev = curr
+            curr = sum
+        }
+
         return curr
     }
 }
