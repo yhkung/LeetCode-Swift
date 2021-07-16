63946bc142e48b70c5c388b75e1ed09dd273680d
YHKung <bowser.kung@gmail.com>
2021-07-16 18:34:06 +0800

    Practice problem #035

--- src/035-Search-Insert-Position.swift
+++ src/035-Search-Insert-Position.swift
@@ -2,16 +2,46 @@
 /// - Time Complexity: O(log(n))
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var low = 0
-        var high = nums.count
-        while low < high {
-            let mid = low + (high - low) / 2
-            if target > nums[mid] {
-                low = mid + 1
+        return binarySearch(nums, target)
+        // return recursionBinarySearch(nums, 0, nums.count - 1, target)
+    }
+
+    private func recursionBinarySearch(_ nums: [Int], _ start: Int, _ end: Int, _ target: Int) -> Int {
+        if start > end {
+            return start
+        }
+
+        let mid = start + (end - start)/2
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[mid] < target {
+            return recursionBinarySearch(nums, mid + 1, end, target)
+        }
+
+        return recursionBinarySearch(nums, start, mid - 1, target)
+    }
+
+    private func binarySearch(_ nums: [Int], _ target: Int) -> Int {
+        var start = 0
+        var end = nums.count
+
+        while start < end {
+            let mid = start + (end - start)/2
+
+            if nums[mid] == target {
+                return mid
+            }
+            if nums[mid] < target {
+                start = mid + 1
             } else {
-                high = mid
+                end = mid
             }
         }
-        return low
+
+        return start
     }
+
 }
