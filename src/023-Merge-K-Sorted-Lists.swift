454ae44b0d027cf58ec2864b2a4f196821b822de
yhkung <bowser.kung@gmail.com>
2022-02-04 00:06:01 +0800

    Practice #023

--- src/023-Merge-K-Sorted-Lists.swift
+++ src/023-Merge-K-Sorted-Lists.swift
@@ -70,3 +70,64 @@ class Solution {
         return mergedHead.next
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+        return mergeLists(lists, 0, lists.count - 1)
+    }
+
+    private func mergeKLists(_ lists: [ListNode?], _ start: Int, _ end: Int) -> ListNode? {
+        guard start <= end else {
+            return nil
+        }
+        if start == end {
+            return lists[start]
+        }
+
+        let mid = (end - start) / 2 + start
+
+        var list1: ListNode? = mergeLists(lists, start, mid)
+        var list2: ListNode? = mergeLists(lists, mid + 1, end)
+
+        if list1 == nil {
+            return list2
+        }
+
+        if list2 == nil {
+            return list1
+        }
+
+        var dummy: ListNode = ListNode(0)
+        var merged: ListNode? = dummy
+
+        while let value1 = list1?.val, let value2 = list2?.val {
+            if value1 < value2 {
+                merged?.next = list1
+                list1 = list1?.next
+            } else {
+                merged?.next = list2
+                list2 = list2?.next
+            }
+            merged = merged?.next
+        }
+
+        if list1 == nil {
+            merged?.next = list2
+        }
+        if list2 == nil {
+            merged?.next = list1
+        }
+
+        return dummy.next
+    }
+}
