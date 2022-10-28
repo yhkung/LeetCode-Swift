f6eed22d32bc46d7fce887c8073532f028fff1ae
Ian Kung <yhkung@ians-mbp.lan>
2022-10-28 00:22:35 -0700

    Practice #200 with bfs & dfs algorithm

--- src/200-Number-of-Islands.swift
+++ src/200-Number-of-Islands.swift
@@ -4,36 +4,71 @@
 /// - Space Complexity: O(min(m, n))
 class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
-        let m = grid.count
-        let n = grid.first?.count ?? 0
-        var grid = grid
-        var islands = 0
-        let offsets = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-        for r in 0..<m {
-            for c in 0..<n {
-                if grid[r][c] == "1" {
-                    islands += 1
-                    grid[r][c] = "0"
+        guard !grid.isEmpty else { 
+            return 0
+        }
 
-                    var queue: [(Int, Int)] = []
-                    queue.append((r, c))
+        let rows = grid.count
+        let cols = grid.first?.count ?? 0
+        var visited = Array(repeating: Array(repeating: false, count: cols), count: rows)
+        var numberOfIslands = 0
 
-                    while !queue.isEmpty {
-                        let cur = queue.removeFirst()
-                        for offset in offsets {
-                            var row = cur.0 + offset.0
-                            var col = cur.1 + offset.1
-                            let isValid = row >= 0 && row < m && col >= 0 && col < n
-                            if isValid && grid[row][col] == "1" {
-                                grid[row][col] = "0"
-                                queue.append((row, col))
-                            }
-                        }
-                    }
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if grid[i][j] == "1" && !visited[i][j] {
+                    dfs(grid, i, j, &visited)
+                    numberOfIslands += 1
                 }
+            }
+        }        
 
+        return numberOfIslands
+    }
+
+    private func bfs(_ grid: [[Character]], _ row: Int, _ col: Int, _ visited: inout [[Bool]]) {
+        let rows = grid.count
+        let cols = grid.first?.count ?? 0
+
+        var queue = [(Int, Int)]()
+        visited[row][col] = true
+        queue.append((row, col))
+
+        while !queue.isEmpty {
+            let (r, c) = queue.removeFirst()
+            let directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+            for dir in directions {
+                let nextRow = r + dir.0
+                let nextCol = c + dir.1
+
+                if nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == "1" {
+                    queue.append((nextRow, nextCol))
+                    visited[nextRow][nextCol] = true
+                }
             }
         }
-        return islands
+    }
+
+    private func dfs(_ grid: [[Character]], _ row: Int, _ col: Int, _ visited: inout [[Bool]]) {
+        let rows = grid.count
+        let cols = grid.first?.count ?? 0
+
+        guard row >= 0 && row < rows && col >= 0 && col < cols else {
+            return
+        }        
+        if visited[row][col] {
+            return
+        }
+        if grid[row][col] == "0" {
+            return
+        }
+
+        visited[row][col] = true
+
+        let directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        for dir in directions {
+            let nextRow = row + dir.0
+            let nextCol = col + dir.1
+            dfs(grid, nextRow, nextCol, &visited)
+        }
     }
 }
