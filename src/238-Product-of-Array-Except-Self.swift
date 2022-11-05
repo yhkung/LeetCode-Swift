ba8344aeaaefbf6287e7d651bede2ab49267ccdb
Ian Kung <yhkung@ians-mbp.lan>
2022-11-05 00:40:33 -0700

    add #238

--- src/238-Product-of-Array-Except-Self.swift
+++ src/238-Product-of-Array-Except-Self.swift
@@ -37,37 +37,59 @@ class Solution {
 /// - Approach: Left & Right product lists
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var n = nums.count
-        var left: [Int] = Array(repeating: 1, count: n)
-        var right: [Int] = Array(repeating: 1, count: n)
-        for i in 1..<n {
-            left[i] = left[i - 1] * nums[i]
-        }
-        for i in stride(from: n - 2, through: 0, by: -1) {
-            right[i] = right[i + 1] * nums[i + 1]
-        }
-        var product: [Int] = []
+        let n = nums.count
+        var left = Array(repeating: 0, count: n)
+        var right = Array(repeating: 0, count: n)
+
         for i in 0..<n {
-            product = left[i] * right[i]
+            if i > 0 {
+                left[i] = left[i - 1] * nums[i]
+            } else {
+                left[i] = nums[i]
+            }
         }
-        return product
+
+        for i in (0..<n).reversed() {
+            if i == n - 1 {
+                right[i] = nums[i]
+            } else {
+                right[i] = right[i + 1] * nums[i]
+            }
+        }
+
+        var result = Array(repeating: 0, count: n)
+        for i in 0..<n {
+            if i == 0 {
+                result[i] = right[i + 1]
+            } else if i == n - 1 {
+                result[i] = left[i - 1]
+            } else {
+                result[i] = left[i - 1] * right[i + 1]
+            }
+        }
+        
+        return result
     }
 }
 
 /// - Approach: O(1) space approach
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var n = nums.count
-        var products: [Int] = Array(repeating: 1, count: n)
-        for i in 1..<n {
-            products[i] = products[i - 1] * nums[i - 1]
+        let n = nums.count        
+        var result = Array(repeating: 1, count: n)
+
+        var prefix = 1
+        for i in 0..<n {
+            result[i] = prefix
+            prefix *= nums[i]
+        }        
+        
+        var postfix = 1
+        for i in (0..<n).reversed() {
+            result[i] *= postfix
+            postfix *= nums[i]
         }
 
-        var r = 1
-        for i in stride(from: n - 1, through: 0, by: -1) {
-            products[i] = products[i] * r
-            r *= nums[i]
-        }
-        return products
+        return result        
     }
 }
