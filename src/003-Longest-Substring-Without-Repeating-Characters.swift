80a02c0cd08420aeac47dc82ab13af8ff2eeef5a
Ian Kung <yhkung@ians-mbp.lan>
2022-11-06 01:00:03 -0700

    Add #003

--- src/003-Longest-Substring-Without-Repeating-Characters.swift
+++ src/003-Longest-Substring-Without-Repeating-Characters.swift
@@ -1,58 +1,22 @@
 /// 003. Longest Substring Without Repeating Characters
 /// - Approach: Sliding Window
-/// - Time Complexity: O(n)
+/// - Time Complexity: O(n), Space: O(n)
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        guard !s.isEmpty else {
-            return 0
-        }
         let s = Array(s)
-
-        var maxLength = 0
-        var start = 0
-        var end = 0
-        let n = s.count
-        var memo = Set<Character>()
-
-        while end < n {
-            if memo.contains(s[end]) {
-                memo.remove(s[start])
-                start += 1
-            } else {
-                memo.insert(s[end])
-                maxLength = max(maxLength, end - start + 1)
-                end += 1
+        var charSet = Set<Character>()
+        var left = 0        
+        var longest = 0
+        
+        for right in 0..<s.count {
+            while charSet.contains(s[right]) {
+                charSet.remove(s[left])
+                left += 1
             }
-        }
-        return maxLength
-    }
-}
-
-class Solution {
-    func lengthOfLongestSubstring(_ s: String) -> Int {
-        guard !s.isEmpty else {
-            return 0
+            charSet.insert(s[right])
+            longest = max(longest, right - left + 1)
         }
 
-        let s = Array(s)
-
-        var maxLength = 0
-        var start = 0
-        var end = 0
-        let n = s.count
-        var indexes = [Character: Int]()
-
-        while end < n {
-            let next = s[end]
-            if let index = indexes[next] {
-                start = max(start, index)
-            }
-
-            maxLength = max(maxLength, end - start + 1)
-            indexes[s[end]] = end + 1
-            end += 1
-        }
-
-        return maxLength
+        return longest
     }
 }
