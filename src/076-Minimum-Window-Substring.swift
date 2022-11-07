08d7e94ad7dc48a3b50b1011c9cb18544b56dd23
Ian Kung <yhkung@ians-mbp.lan>
2022-11-06 16:10:35 -0800

    Add #076

--- src/076-Minimum-Window-Substring.swift
+++ src/076-Minimum-Window-Substring.swift
@@ -38,3 +38,49 @@ class Solution {
         return minLength == Int.max ? "" : String(s[minStart..<minStart + minLength])
     }
 }
+
+class Solution {
+    func minWindow(_ s: String, _ t: String) -> String {
+        if t.isEmpty { return "" }
+
+        let s = Array(s)
+        let t = Array(t)
+        var countT = t.reduce(into: [Character: Int]()) { $0[$1, default: 0] += 1 }
+        var window = [Character: Int]()
+        
+        var res: (Int, Int) = (0, 0)
+        var minLength = Int.max
+        var left = 0
+        var have = 0
+        var need = countT.count
+
+        for right in 0..<s.count {
+            window[s[right], default: 0] += 1
+
+            if let count = countT[s[right]], count == window[s[right], default: 0] {
+                have += 1
+            }
+
+            while have == need {
+                if right - left + 1 < minLength {
+                    res = (left, right)
+                    minLength = right - left + 1
+                }
+
+                window[s[left], default: 0] -= 1
+
+                if let count = countT[s[left]], window[s[left], default: 0] < count {
+                    have -= 1
+                }
+
+                left += 1
+            }
+        }
+        
+        if minLength == Int.max {
+            return ""
+        }
+
+        return String(s[res.0...res.1])
+    }
+}
